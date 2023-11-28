f4b8a8bba0d90d09cdd72b05750cc8111e5501a5
Arthur Saito <arthur_saito1998@hotmail.com>
2023-11-28 20:17:28 -0300

    [chore] moving exceptions to controller

--- src/Services/BanksFacade.cc
+++ src/Services/BanksFacade.cc
@@ -7,28 +7,26 @@ void BanksFacade::getAllBanks(std::function<void(const BankResponse&)> callback)
    req->setPath("/banks/v1");
 
    httpClient->sendRequest(req, [this, callback](drogon::ReqResult result, const drogon::HttpResponsePtr& response) {
-        if (result == drogon::ReqResult::Ok && response->getStatusCode() == drogon::HttpStatusCode::k200OK) {
-            Json::Reader reader;
-            Json::Value jsonResponse;
-            std::string responseBody = std::string(response->getBody());
-            reader.parse(responseBody, jsonResponse);
+        Json::Reader reader;
+        Json::Value jsonResponse;
+        std::string responseBody = std::string(response->getBody());
+        reader.parse(responseBody, jsonResponse);
 
-            BankResponse bankResponse;
-            for (const auto& jsonBank : jsonResponse) {
-                Bank bank;
-                bank.ispb = jsonBank["ispb"].asString();
-                bank.name = jsonBank["name"].asString();
-                if (!jsonBank["code"].isNull()) {
-                    bank.code = jsonBank["code"].asInt();
-                }
-                bank.fullname = jsonBank["fullName"].asString();
-                bankResponse.banks.push_back(bank);
+        BankResponse bankResponse;
+        for (const auto& jsonBank : jsonResponse) {
+            Bank bank;
+            bank.ispb = jsonBank["ispb"].asString();
+            bank.name = jsonBank["name"].asString();
+            if (!jsonBank["code"].isNull()) {
+                bank.code = jsonBank["code"].asInt();
             }
+            bank.fullname = jsonBank["fullName"].asString();
+            bankResponse.banks.push_back(bank);
+        }
 
-            callback(bankResponse);
-        } 
+        callback(bankResponse);
+        
     });
-
 };
 
 void BanksFacade::getBanksByCode(int code, std::function<void(const Json::Value&)> callback) {
