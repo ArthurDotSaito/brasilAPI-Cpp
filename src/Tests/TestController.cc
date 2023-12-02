0ad354c659e8724e53f937143d90b978fbab28b9
Arthur Saito <arthur_saito1998@hotmail.com>
2023-12-02 15:18:19 -0300

    [minor changes] TestController

--- src/Tests/TestController.cc
+++ src/Tests/TestController.cc
@@ -3,27 +3,14 @@
 class TestController : public drogon::HttpController<TestController> {
 public:
     METHOD_LIST_BEGIN
-    ADD_METHOD_TO(TestController::handleGet, "/api/{1}", drogon::Get);
-    ADD_METHOD_TO(TestController::handlePost, "/api/{1}", drogon::Post);
+    ADD_METHOD_TO(TestController::handleGetAllBanks, "/api/banks/v1", drogon::Get);
     METHOD_LIST_END
 
-    void handleGet(const drogon::HttpRequestPtr &req, 
+    void handleGetAllBanks(const drogon::HttpRequestPtr &req, 
     std::function<void(const drogon::HttpResponsePtr &)> &&callback) {
-        auto path = req->getPath();
         auto resp = drogon::HttpResponse::newHttpResponse();
-
-        if (path == "/api/banks/v1") {
-            resp->setBody("Response for /banks/v1");
-        }
-
+        resp->setBody(R"([{"ispb":"00000000","name":"Test Bank","code":123,"fullName":"Test Bank Ltd."}])");
         callback(resp);
     }
 
-    void handlePost(const drogon::HttpRequestPtr &req, 
-    std::function<void(const drogon::HttpResponsePtr &)> &&callback) {
-        auto path = req->getPath();
-        auto resp = drogon::HttpResponse::newHttpResponse();
-
-        callback(resp);
-    }
 };
