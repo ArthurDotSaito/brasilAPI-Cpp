0e4028c5fb73a17c94d6a1eb01b98b6fee39175e
Arthur Saito <arthur_saito1998@hotmail.com>
2023-12-02 16:02:20 -0300

    [major change] moving tests to gtest

---
+++ src/Tests/Services/BanksTest.cc
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include "Client/BrasilAPIClient.h"
+
+class BrasilAPIClientTests : public ::testing::Test {
+protected:
+    BrasilAPIClient brasilAPI;
+
+    BrasilAPIClientTests() {
+        brasilAPI.setUserAgent("MeuApp/1.0");
+    }
+};
+
+TEST_F(BrasilAPIClientTests, TestGetAllBanks) {
+    std::promise<BankResponse> promise;
+    auto future = promise.get_future();    
+
+    brasilAPI.getAllBanks([&promise](const BankResponse& response) {
+        promise.set_value(response);
+    });
+
+    future.wait();
+    auto response = future.get();
+
+    EXPECT_FALSE(response.banks.empty());
+}
+
+
