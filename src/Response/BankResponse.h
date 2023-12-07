63d607c2d2d9f5cb56626c02f1bdb92df0a10c49
Arthur Saito <arthur_saito1998@hotmail.com>
2023-12-06 21:05:03 -0300

    making more simple

--- src/Response/BankResponse.h
+++ src/Response/BankResponse.h
@@ -1,29 +1,22 @@
 #ifndef BANK_RESPONSE_H
 #define BANK_RESPONSE_H
 
-#include<string>
-#include<optional> 
-#include<vector>
+#include <string>
+#include <vector>
 #include <json/json.h>
 #include "BaseResponse.h"
 
-class Bank:public BaseResponse {
+class Bank : public BaseResponse {
 public:
     std::string ispb;
     std::string name;
     std::optional<int> code;
     std::string fullName;
-
-    Json::Value toJsonValue() const;
-    static Bank fromJsonValue(const Json::Value& jsonValue);
 };
 
-class BankResponse: public BaseResponse{
+class BankResponse : public BaseResponse {
 public:
     std::vector<Bank> banks;
-    Json::Value toJsonValue() const;
-    static BankResponse fromJsonValue(const Json::Value& jsonValue);
 };
-
 
 #endif // BANK_RESPONSE_H
--- src/Services/BanksFacade.cc
+++ src/Services/BanksFacade.cc
@@ -10,43 +10,60 @@ void BanksFacade::getAllBanks(std::function<void(const BankResponse&)> callback)
     
     httpClient->sendRequest(req, [this, callback, fullUrl](drogon::ReqResult result, const drogon::HttpResponsePtr& response) {
         ensureSuccess(response, "api/banks/v1");
-        Json::Reader reader;
-        Json::Value jsonResponse;
         std::string responseBody = std::string(response->getBody());
 
-        if (!reader.parse(responseBody, jsonResponse)) {
-            std::cerr << "Error during JSON parsing: " << responseBody << std::endl;
-            return;
-        }
-
-        BankResponse bankResponse = BankResponse::fromJsonValue(jsonResponse);
+        BankResponse bankResponse;
         bankResponse.calledURL = fullUrl;
         bankResponse.jsonResponse = responseBody;
+
+        Json::Value jsonResponse;
+        Json::Reader reader;
+        if (reader.parse(responseBody, jsonResponse) && jsonResponse.isArray()) {
+            for (const auto& jsonBank : jsonResponse) {
+                Bank bank;
+                bank.ispb = jsonBank["ispb"].asString();
+                bank.name = jsonBank["name"].asString();
+                if (!jsonBank["code"].isNull()) {
+                    bank.code = jsonBank["code"].asInt();
+                }
+                bank.fullName = jsonBank["fullName"].asString();
+                bankResponse.banks.push_back(bank);
+            }
+        }
+
         callback(bankResponse);
     });
 }
+
 
 
 void BanksFacade::getBanksByCode(int code, std::function<void(const Bank&)> callback) {
     auto req = drogon::HttpRequest::newHttpRequest();
     req->setMethod(drogon::HttpMethod::Get);
     req->setPath("/api/banks/v1/" + std::to_string(code));
-    std::string baseUrl = "/api/banks/v1/" + std::to_string(code);
+    std::string fullUrl = baseUrl + req->getPath();
 
-    httpClient->sendRequest(req, [this, callback, baseUrl](drogon::ReqResult result, const drogon::HttpResponsePtr& response) {
+    httpClient->sendRequest(req, [this, callback, fullUrl](drogon::ReqResult result, const drogon::HttpResponsePtr& response) {
         ensureSuccess(response, "api/banks/v1/{code}");
-        Json::Reader reader;
-        Json::Value jsonResponse;
         std::string responseBody = std::string(response->getBody());
 
-        if (!reader.parse(responseBody, jsonResponse)) {
+        Json::Value jsonResponse;
+        Json::Reader reader;
+        Bank bank;
+        if (reader.parse(responseBody, jsonResponse)) {
+            bank.ispb = jsonResponse["ispb"].asString();
+            bank.name = jsonResponse["name"].asString();
+            if (!jsonResponse["code"].isNull()) {
+                bank.code = jsonResponse["code"].asInt();
+            }
+            bank.fullName = jsonResponse["fullName"].asString();
+            bank.calledURL = fullUrl;
+            bank.jsonResponse = responseBody;
+        } else {
             std::cerr << "Error during JSON parsing: " << responseBody << std::endl;
             return;
         }
 
-        Bank bankResponse = Bank::fromJsonValue(jsonResponse);
-        bankResponse.calledURL = baseUrl;
-        bankResponse.jsonResponse = responseBody;
-        callback(bankResponse);
+        callback(bank);
     });
-};
+}
