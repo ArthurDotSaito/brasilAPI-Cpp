40204661444b47eb9b748cf107a4da8b8b123402
Arthur Saito <arthur_saito1998@hotmail.com>
2023-12-13 23:03:46 -0300

    implementing CptecHandler

--- src/Handlers/CptecHandler.h
+++ src/Handlers/CptecHandler.h
@@ -1,7 +1,7 @@
 #include "BrasilAPIHandlerBase.h"
 #include "Response/CptecCidadesResponse.h"
 
-class CorretorasHandler: public BrasilAPIHandlerBase{
+class CptecHandler: public BrasilAPIHandlerBase{
     public:
         void listAllCities(std::function<void(const CptecCidadesResponse&)> callback);
         void searchByTerms(std::string cityName , std::function<void(const CptecCidades&)> callback);
