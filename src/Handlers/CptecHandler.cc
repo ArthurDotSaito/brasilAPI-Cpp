64d0e141bb2efe165ff1ed8c4a4503f7e0cdba81
Arthur Saito <arthur_saito1998@hotmail.com>
2023-12-16 16:38:09 -0300

    more error messages

--- src/Handlers/CptecHandler.cc
+++ src/Handlers/CptecHandler.cc
@@ -10,26 +10,32 @@ void CptecHandler::listAllCities(std::function<void(const CptecCidadesResponse&)
     std::cout << "Iniciando a solicitação para: " << fullUrl << std::endl;
     
     httpClient->sendRequest(req, [this, callback, fullUrl](drogon::ReqResult result, const drogon::HttpResponsePtr& response) {
-        ensureSuccess(response, "/api/cptec/v1/cidade");
-        std::string responseBody = std::string(response->getBody());
+        try{
+            ensureSuccess(response, "/api/cptec/v1/cidade");
+            std::string responseBody = std::string(response->getBody());
 
-        CptecCidadesResponse cptecCidadesResponse;
-        cptecCidadesResponse.calledURL = fullUrl;
-        cptecCidadesResponse.jsonResponse = responseBody;
+            CptecCidadesResponse cptecCidadesResponse;
+            cptecCidadesResponse.calledURL = fullUrl;
+            cptecCidadesResponse.jsonResponse = responseBody;
 
-        Json::Value jsonResponse;
-        Json::Reader reader;
-        if (reader.parse(responseBody, jsonResponse) && jsonResponse.isArray()) {
-            for (const auto& jsonCptec : jsonResponse) {
-                CptecCidades cidade;
-                cidade.nome = jsonCptec["nome"].asString();
-                cidade.estado = jsonCptec["estado"].asString();
-                cidade.id = jsonCptec["id"].asInt();
-                cptecCidadesResponse.cptecCidades.push_back(cidade);
+            Json::Value jsonResponse;
+            Json::Reader reader;
+            if (reader.parse(responseBody, jsonResponse) && jsonResponse.isArray()) 
+            {
+                for (const auto& jsonCptec : jsonResponse) {
+                    CptecCidades cidade;
+                    cidade.nome = jsonCptec["nome"].asString();
+                    cidade.estado = jsonCptec["estado"].asString();
+                    cidade.id = jsonCptec["id"].asInt();
+                    cptecCidadesResponse.cptecCidades.push_back(cidade);
+                }
             }
-        }
 
-        callback(cptecCidadesResponse);
+            callback(cptecCidadesResponse);
+        }catch(const BrasilAPIException& e){
+            std::cerr << "Error: " << e.what() << std::endl;
+            return;
+        }
     });
 }
 
@@ -41,25 +47,31 @@ void CptecHandler::searchByTerms(std::string cityName , std::function<void(const
     std::cout << "Iniciando a solicitação para: " << fullUrl << std::endl;
     
     httpClient->sendRequest(req, [this, callback, fullUrl](drogon::ReqResult result, const drogon::HttpResponsePtr& response) {
-        ensureSuccess(response, "/api/cptec/v1/cidade/{cityName}");
-        std::string responseBody = std::string(response->getBody());
+        try{
+            ensureSuccess(response, "/api/cptec/v1/cidade/{cityName}");
+            std::string responseBody = std::string(response->getBody());
 
-        CptecCidadesResponse cptecCidadesResponse;
-        cptecCidadesResponse.calledURL = fullUrl;
-        cptecCidadesResponse.jsonResponse = responseBody;
+            CptecCidadesResponse cptecCidadesResponse;
+            cptecCidadesResponse.calledURL = fullUrl;
+            cptecCidadesResponse.jsonResponse = responseBody;
 
-        Json::Value jsonResponse;
-        Json::Reader reader;
-        if (reader.parse(responseBody, jsonResponse) && jsonResponse.isArray()) {
-            for (const auto& jsonCptec : jsonResponse) {
-                CptecCidades cidade;
-                cidade.nome = jsonCptec["nome"].asString();
-                cidade.estado = jsonCptec["estado"].asString();
-                cidade.id = jsonCptec["id"].asInt();
-                cptecCidadesResponse.cptecCidades.push_back(cidade);
+            Json::Value jsonResponse;
+            Json::Reader reader;
+            if (reader.parse(responseBody, jsonResponse) && jsonResponse.isArray()) 
+            {
+                for (const auto& jsonCptec : jsonResponse) {
+                    CptecCidades cidade;
+                    cidade.nome = jsonCptec["nome"].asString();
+                    cidade.estado = jsonCptec["estado"].asString();
+                    cidade.id = jsonCptec["id"].asInt();
+                    cptecCidadesResponse.cptecCidades.push_back(cidade);
+                }
             }
+            callback(cptecCidadesResponse);
+        }catch(const BrasilAPIException& e){
+            std::cerr << "Error: " << e.what() << std::endl;
+            return;
         }
-        callback(cptecCidadesResponse);
     });
 }
 
@@ -71,34 +83,41 @@ void CptecHandler::getCapitais(std::function<void(const CptecCapitaisResponse&)>
     std::cout << "Iniciando a solicitação para: " << fullUrl << std::endl;
     
     httpClient->sendRequest(req, [this, callback, fullUrl](drogon::ReqResult result, const drogon::HttpResponsePtr& response) {
-        ensureSuccess(response, "/api/cptec/v1/clima/capital");
-        std::string responseBody = std::string(response->getBody());
+        try{
+            ensureSuccess(response, "/api/cptec/v1/clima/capital");
+            std::string responseBody = std::string(response->getBody());
 
-        CptecCapitaisResponse cptecCapitaisResponse;
-        cptecCapitaisResponse.calledURL = fullUrl;
-        cptecCapitaisResponse.jsonResponse = responseBody;
+            CptecCapitaisResponse cptecCapitaisResponse;
+            cptecCapitaisResponse.calledURL = fullUrl;
+            cptecCapitaisResponse.jsonResponse = responseBody;
 
-        Json::Value jsonResponse;
-        Json::Reader reader;
-        if (reader.parse(responseBody, jsonResponse) && jsonResponse.isArray()) {
-            for (const auto& jsonCptec : jsonResponse) {
-                CptecCapitais capitais;
+            Json::Value jsonResponse;
+            Json::Reader reader;
+            if (reader.parse(responseBody, jsonResponse) && jsonResponse.isArray()) 
+            {
+                for (const auto& jsonCptec : jsonResponse) {
+                    CptecCapitais capitais;
 
-                capitais.codigo_icao = jsonCptec["codigo_icao"].asString();
-                capitais.atualizado_em = jsonCptec["atualizado_em"].asString();
-                capitais.pressao_atmosferica = jsonCptec["pressao_atmosferica"].asString();
-                capitais.visibilidade = jsonCptec["visibilidade"].asString();
-                capitais.vento = jsonCptec["vento"].asInt();
-                capitais.direcao_vento = jsonCptec["direcao_vento"].asInt();
-                capitais.umidade = jsonCptec["umidade"].asInt();
-                capitais.condicao = jsonCptec["condicao"].asString();
-                capitais.condicao_Desc = jsonCptec["condicao_Desc"].asString();
-                capitais.temp = jsonCptec["temp"].asInt();
+                    capitais.codigo_icao = jsonCptec["codigo_icao"].asString();
+                    capitais.atualizado_em = jsonCptec["atualizado_em"].asString();
+                    capitais.pressao_atmosferica = jsonCptec["pressao_atmosferica"].asString();
+                    capitais.visibilidade = jsonCptec["visibilidade"].asString();
+                    capitais.vento = jsonCptec["vento"].asInt();
+                    capitais.direcao_vento = jsonCptec["direcao_vento"].asInt();
+                    capitais.umidade = jsonCptec["umidade"].asInt();
+                    capitais.condicao = jsonCptec["condicao"].asString();
+                    capitais.condicao_Desc = jsonCptec["condicao_Desc"].asString();
+                    capitais.temp = jsonCptec["temp"].asInt();
 
-                cptecCapitaisResponse.cptecCapitais.push_back(capitais);
+                    cptecCapitaisResponse.cptecCapitais.push_back(capitais);
+                }
             }
+            callback(cptecCapitaisResponse);
+
+        }catch(const BrasilAPIException& e){
+            std::cerr << "Error: " << e.what() << std::endl;
+            return;
         }
-        callback(cptecCapitaisResponse);
     });
 }
 
