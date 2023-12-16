ebd9ab5d20dccd41511e10a8ba40ebe1915317a8
Arthur Saito <arthur_saito1998@hotmail.com>
2023-12-15 22:22:52 -0300

    implementing cptecCapitais

--- src/Handlers/CptecHandler.cc
+++ src/Handlers/CptecHandler.cc
@@ -62,3 +62,41 @@ void CptecHandler::searchByTerms(std::string cityName , std::function<void(const
         callback(cptecCidadesResponse);
     });
 }
+
+void CptecHandler::getCapitais(std::function<void(const CptecCapitaisResponse&)> callback) {
+    auto req = drogon::HttpRequest::newHttpRequest();
+    req->setMethod(drogon::HttpMethod::Get);
+    req->setPath("/api/cptec/v1/clima/capital");
+    std::string fullUrl = baseUrl + req->getPath();
+    std::cout << "Iniciando a solicitação para: " << fullUrl << std::endl;
+    
+    httpClient->sendRequest(req, [this, callback, fullUrl](drogon::ReqResult result, const drogon::HttpResponsePtr& response) {
+        ensureSuccess(response, "/api/cptec/v1/clima/capital");
+        std::string responseBody = std::string(response->getBody());
+
+        CptecCapitaisResponse cptecCapitaisResponse;
+        cptecCapitaisResponse.calledURL = fullUrl;
+        cptecCapitaisResponse.jsonResponse = responseBody;
+
+        Json::Value jsonResponse;
+        Json::Reader reader;
+        if (reader.parse(responseBody, jsonResponse) && jsonResponse.isArray()) {
+            for (const auto& jsonCptec : jsonResponse) {
+                CptecCapitais capitais;
+                capitais.codigo_icao = jsonCptec["codigo_icao"].asString();
+                capitais.atualizado_em = jsonCptec["atualizado_em"].asString();
+                capitais.pressao_atmosferica = jsonCptec["pressao_atmosferica"].asString();
+                capitais.visibilidade = jsonCptec["visibilidade"].asString();
+                capitais.vento = jsonCptec["vento"].asInt();
+                capitais.direcao_vento = jsonCptec["direcao_vento"].asInt();
+                capitais.umidade = jsonCptec["umidade"].asInt();
+                capitais.condicao = jsonCptec["condicao"].asString();
+                capitais.condicao_Desc = jsonCptec["condicao_Desc"].asString();
+                capitais.temp = jsonCptec["temp"].asInt();
+                cptecCapitaisResponse.cptecCapitais.push_back(capitais);
+            }
+        }
+
+        callback(cptecCapitaisResponse);
+    });
+}
