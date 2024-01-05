239f8d6592c65047a5014238933735652ccd45ae
Arthur Saito <arthur_saito1998@hotmail.com>
2024-01-04 23:37:13 -0300

    feat: implementing listFipePreco method

--- src/Handlers/FipeHandler.cc
+++ src/Handlers/FipeHandler.cc
@@ -43,3 +43,50 @@ void FipeHandler::listFipeMarcas(const std::optional<std::string> &tipoVeiculo, 
     callback(fipeResponse);
   });
 }
+
+void FipeHandler::listFipePreco(const std::string &codigoFipe, const std::optional<int> &tabela_referencia,
+    std::function<void(const FipePrecos &)> callback) {
+  auto req = drogon::HttpRequest::newHttpRequest();
+  req->setMethod(drogon::HttpMethod::Get);
+  std::stringstream pathStream;
+  std::stringstream queryStream;
+  pathStream << "/api/fipe/preco/v1/" + codigoFipe;
+
+  if (tabela_referencia) {
+    queryStream << "?tabela_referencia=" << *tabela_referencia;
+  };
+
+  req->setPath(pathStream.str() + queryStream.str());
+  std::string fullUrl = baseUrl + req->getPath();
+  std::cout << "Iniciando a solicitação para: " << fullUrl << std::endl;
+
+  httpClient->sendRequest(req, [this, callback, fullUrl](drogon::ReqResult result, const drogon::HttpResponsePtr &response) {
+    ensureSuccess(response, fullUrl);
+    std::string responseBody = std::string(response->getBody());
+
+    FipePrecos precosResponse;
+    precosResponse.calledURL = fullUrl;
+    precosResponse.jsonResponse = responseBody;
+
+    Json::Value jsonResponse;
+    Json::Reader reader;
+    if (reader.parse(responseBody, jsonResponse) && jsonResponse.isArray()) {
+      for (const auto &jsonItem : jsonResponse) {
+        Precos precos;
+        precos.setValor(jsonItem["valor"].asString());
+        precos.setMarca(jsonItem["marca"].asString());
+        precos.setModelo(jsonItem["modelo"].asString());
+        precos.setAnoModelo(jsonItem["ano_modelo"].asInt());
+        precos.setCombustivel(jsonItem["combustivel"].asString());
+        precos.setCodigoFipe(jsonItem["codigo_fipe"].asString());
+        precos.setMesReferencia(jsonItem["mes_referencia"].asString());
+        precos.setTipoVeiculo(jsonItem["tipo_veiculo"].asInt());
+        precos.setSiglaCombustivel(jsonItem["sigla_combustivel"].asString());
+
+        precosResponse.precos.push_back(precos);
+      }
+    }
+
+    callback(precosResponse);
+  });
+}
--- src/Handlers/FipeHandler.h
+++ src/Handlers/FipeHandler.h
@@ -5,4 +5,6 @@ class FipeHandler : public BrasilAPIHandlerBase {
   public:
   void listFipeMarcas(const std::optional<std::string> &tipoVeiculo, const std::optional<int> &tabela_referencia,
       std::function<void(const FipeMarcas &)> callback);
+  void listFipePreco(const std::string &codigoFipe, const std::optional<int> &tabela_referencia,
+      std::function<void(const FipePrecos &)> callback);
 };
