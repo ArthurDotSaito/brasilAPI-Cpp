c8ea98073608f41bb2088e273cef0a01308e3da5
Arthur Saito <arthur_saito1998@hotmail.com>
2024-01-08 19:22:15 -0300

    fixing previsaoOceanica

--- src/Response/PrevisaoOceanica.h
+++ src/Response/PrevisaoOceanica.h
@@ -2,12 +2,13 @@
 #define CPTECOCEANO_RESPONSE_H
 
 #include "BaseResponse.h"
+#include "Utils/JsonSerialize.h"
 #include <json/json.h>
 #include <string>
 #include <vector>
 
 class DadosOnda {
-  public:
+  private:
   float vento;
   std::string direcao_vento;
   std::string direcao_vento_desc;
@@ -16,6 +17,57 @@ class DadosOnda {
   std::string direcao_onda_desc;
   std::string agitation;
   std::string hora;
+
+  public:
+  float getVento() const {
+    return vento;
+  }
+  const std::string &getDirecaoVento() const {
+    return direcao_vento;
+  }
+  const std::string &getDirecaoVentoDesc() const {
+    return direcao_vento_desc;
+  }
+  float getAlturaOnda() const {
+    return altura_onda;
+  }
+  const std::string &getDirecaoOnda() const {
+    return direcao_onda;
+  }
+  const std::string &getDirecaoOndaDesc() const {
+    return direcao_onda_desc;
+  }
+  const std::string &getAgitation() const {
+    return agitation;
+  }
+  const std::string &getHora() const {
+    return hora;
+  }
+
+  void setVento(float v) {
+    vento = v;
+  }
+  void setDirecaoVento(const std::string &dv) {
+    direcao_vento = dv;
+  }
+  void setDirecaoVentoDesc(const std::string &dvd) {
+    direcao_vento_desc = dvd;
+  }
+  void setAlturaOnda(float a) {
+    altura_onda = a;
+  }
+  void setDirecaoOnda(const std::string &do_) {
+    direcao_onda = do_;
+  }
+  void setDirecaoOndaDesc(const std::string &dod) {
+    direcao_onda_desc = dod;
+  }
+  void setAgitation(const std::string &a) {
+    agitation = a;
+  }
+  void setHora(const std::string &h) {
+    hora = h;
+  }
 
   Json::Value toJson() const {
     Json::Value jsonDadosOnda;
@@ -32,9 +84,24 @@ class DadosOnda {
 };
 
 class Onda {
-  public:
+  private:
   std::string data;
   std::vector<DadosOnda> dados_ondas;
+
+  public:
+  const std::string &getData() const {
+    return data;
+  }
+  const std::vector<DadosOnda> &getDadosOndas() const {
+    return dados_ondas;
+  }
+
+  void setData(const std::string &d) {
+    data = d;
+  }
+  void addClimaDiario(const DadosOnda &dadosOnda) {
+    dados_ondas.push_back(dadosOnda);
+  }
 
   Json::Value toJson() const {
     Json::Value jsonOnda;
@@ -48,48 +115,59 @@ class Onda {
   }
 };
 
-class CptecPrevisaoOceanica : public BaseResponse {
-  public:
+class CptecPrevisaoOceanica : public BaseResponse, public JsonSerialize {
+  private:
   std::string cidade;
   std::string estado;
   std::string atualizado_em;
   std::vector<Onda> ondas;
 
-  Json::Value toJson() const {
-    Json::Value cptecPrevisaoOceanica;
-    cptecPrevisaoOceanica["cidade"] = cidade;
-    cptecPrevisaoOceanica["estado"] = estado;
-    cptecPrevisaoOceanica["atualizado_em"] = atualizado_em;
-
-    Json::Value jsonOndasArray(Json::arrayValue);
-    for (const auto &onda : ondas) {
-      jsonOndasArray.append(onda.toJson());
-    }
-    cptecPrevisaoOceanica["ondas"] = jsonOndasArray;
-    return cptecPrevisaoOceanica;
+  public:
+  const std::string &getCidade() const {
+    return cidade;
+  }
+  const std::string &getEstado() const {
+    return estado;
+  }
+  const std::string &getAtualizadoEm() const {
+    return atualizado_em;
+  }
+  const std::vector<Onda> &getOndas() const {
+    return ondas;
   }
 
-  std::string serialize() const {
-    Json::StreamWriterBuilder builder;
-    builder["commentStyle"] = "None";
-    builder["indentation"] = "  ";
-    return Json::writeString(builder, this->toJson());
+  void setCidade(const std::string &c) {
+    cidade = c;
+  }
+  void setEstado(const std::string &e) {
+    estado = e;
+  }
+  void setAtualizadoEm(const std::string &ae) {
+    atualizado_em = ae;
+  }
+  void addOnda(const Onda &onda) {
+    ondas.push_back(onda);
+  }
+
+  Json::Value toJson() const override {
+    Json::Value jsonRoot(Json::arrayValue);
+    for (const auto &item : ondas) {
+      jsonRoot.append(item.toJson());
+    }
+    return jsonRoot;
   }
 };
 
-class CptecPrevisaoOceanicaResponse : public BaseResponse {
+class CptecPrevisaoOceanicaResponse : public BaseResponse, public JsonSerialize {
   public:
   std::vector<CptecPrevisaoOceanica> cptecPrevisaoOceanica;
 
-  std::string serialize() const {
-    Json::Value jsonRoot;
-    for (const auto &cptecPrevisaoOceanica : cptecPrevisaoOceanica) {
-      jsonRoot.append(cptecPrevisaoOceanica.toJson());
+  Json::Value toJson() const override {
+    Json::Value jsonRoot(Json::arrayValue);
+    for (const auto &item : cptecPrevisaoOceanica) {
+      jsonRoot.append(item.toJson());
     }
-    Json::StreamWriterBuilder builder;
-    builder["commentStyle"] = "None";
-    builder["indentation"] = "  ";
-    return Json::writeString(builder, jsonRoot);
+    return jsonRoot;
   }
 };
 
